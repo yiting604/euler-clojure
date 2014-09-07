efd762cb288b2557f51c44fa1974bff07354908b
Yiting Zhang <yiting604@gmail.com>
2014-09-07 23:34:56 +0000

    solved p8

---
+++ test/euler/p8_test.clj
@@ -0,0 +1,17 @@
+(ns euler.p8-test
+  (:require [clojure.test :refer :all]
+            [euler.p8 :refer :all]))
+
+(deftest n-product-test
+  (testing "product of first 5 numbers in [1 2 3 4 5 6] is 120."
+    (is (= 120 (n-product 5 [1 2 3 4 5 6]))))
+  (testing "product of first 5 numbers in [1 2 3 4] is 24."
+    (is (= 24 (n-product 5 [1 2 3 4]))))
+  )
+
+(deftest n-max-test
+  (testing "max product of 3 numbers in a row in [1 2 3 4 5 6] is 120."
+    (is (= 120 (n-max 3 [1 2 3 4 5 6] 0))))
+  (testing "max product of 3 numbers in a row in [6 5 4 3 2 1] is 120"
+    (is (= 120 (n-max 3 [6 5 4 3 2 1] 0))))
+  )
